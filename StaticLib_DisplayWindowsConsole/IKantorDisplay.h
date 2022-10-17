09949278644638b4b66af1b2284f611607a3b625
Anna Nobis <anianobis@outlook.com>
2022-10-17 19:28:04 +0200

    displayScreens 1commit

---
+++ StaticLib_DisplayWindowsConsole/IKantorDisplay.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "../include_libraries/tabulate.hpp"
+#include "../Kantorex_login/Kantorex.hpp"
+
+using namespace tabulate;
+
+class IKantorDisplay
+{
+public:
+	void displayLogin();
+	
+	void displayCurrencyTable();
+
+	void displayMenu();
+
+	void reset(); //wraca do menu
+
+	void dislayBalanceTable();
+
+	void displaySellOperation();
+
+	void displayBuyOperation();
+
+	void displayExitScreen();
+};
+
---
+++ StaticLib_DisplayWindowsConsole/StaticLib_DisplayWindowsConsole.cpp
@@ -0,0 +1,10 @@
+// StaticLib_DisplayWindowsConsole.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnStaticLibDisplayWindowsConsole()
+{
+}
