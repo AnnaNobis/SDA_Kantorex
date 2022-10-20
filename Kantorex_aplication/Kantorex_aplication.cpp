ff54f6ceb62e31275b112b4df0b206c3e50a3383
Julia Witucka <jwitucka@gmail.com>
2022-10-20 12:38:52 +0200

    Buy i sell reader

--- Kantorex_aplication/Kantorex_aplication.cpp
+++ Kantorex_aplication/Kantorex_aplication.cpp
@@ -4,24 +4,31 @@
 #include "StaticLib_DisplayWindowsConsole/Menu_Operations.h"
 #include "../include_libraries/tabulate.hpp"
 #include "../Kantorex_login/Kantorex.hpp"
+#include "JSONLib/ReadBuyRates.h"
+#include "JSONLib/ReadSellRates.h"
 
 using namespace tabulate;
 
 int main()
 {
 
-	IKantorDisplay window;
-	window.displayLogin();
-	system("cls"); //czyœci ekran logowania
+	//IKantorDisplay window;
+	//window.displayLogin();
+	//system("cls"); //czyœci ekran logowania
 
-	// IKantorDisplay window;
-   //window.displayCurrencyTable();
+	//// IKantorDisplay window;
+ //  //window.displayCurrencyTable();
 
-	Menu_Operations screen;
-	screen.displayMenuOperations();
+	//Menu_Operations screen;
+	//screen.displayMenuOperations();
 
-    JSONReader dataReader;
-    dataReader.readCurrencies("JSONTabelaA.json");
-    dataReader.wypisz();
+ //   JSONReader dataReader;
+ //   dataReader.readCurrencies("JSONTabelaA.json");
+ //   dataReader.wypisz();
+	//ReadSellRates r;
+	//r.read("TabelaC.json");
+	//std::cout << "---------------------------------" << std::endl;
+	//ReadBuyRates b;
+	//b.read();
 }
 
